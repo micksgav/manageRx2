18d5a112f81747c6b48df58fedcadf357bfbd410
micksgav <153670740+micksgav@users.noreply.github.com>
2024-01-08 23:44:44 -0500

    Added UI for alternatives

--- src/patientUI/AlternativesUI.java
+++ src/patientUI/AlternativesUI.java
@@ -4,30 +4,38 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.io.IOException;
+
 import javax.swing.JList;
 import javax.swing.SwingConstants;
 
+import inventory.Drug;
 import inventory.drugFinder;
 import utilities.*;
-import javax.swing.JScrollPane;
 
-public class InteractionsUI {
+public class AlternativesUI {
+	
+	public static void main(String args[]) throws IOException {
+		new AlternativesUI("02248808");
+	}
 
 	private JFrame frame;
 	static String[] display;
 
 	/**
 	 * Create the application.
+	 * @throws IOException 
 	 */
-	public InteractionsUI(String[] DINs) {
-		String[][] interactions = getInteractions.arraySearch(DINs);
-		display = new String[interactions.length];
+	public AlternativesUI(String DIN) throws IOException {
+		String[][] alternatives = findAlternatives.findAlternative(DIN);
+		display = new String[alternatives.length];
 		for (int i = 0; i < display.length; i++) {
-
-			display[i] = interactions[i][2];
+			display[i] = alternatives[i][0] + " " + alternatives[i][1];
 		}
 		
 		initialize();
@@ -42,10 +50,10 @@ public class InteractionsUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Potential Interactions:");
-		lblNewLabel.setBounds(10, 11, 659, 14);
+		JLabel lblNewLabel = new JLabel("Potential Alternatives:");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Monospaced", Font.BOLD, 14));
+		lblNewLabel.setBounds(10, 11, 659, 14);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JScrollPane scrollPane = new JScrollPane();
