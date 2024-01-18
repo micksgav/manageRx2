7887bd06f9da5a1b3e6fecbf6f6bde9ec791ebfc
micksgav <153670740+micksgav@users.noreply.github.com>
2024-01-18 01:31:18 -0500

    drug search & loading screen demo

---
+++ src/mainUI/main.java
@@ -0,0 +1,12 @@
+package mainUI;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		orderUI thing = new orderUI();
+		thing.setVisible(true);
+		
+	}
+
+}
